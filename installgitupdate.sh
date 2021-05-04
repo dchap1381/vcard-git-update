28a26abc6a01611e99fd17b352daaca3a5c775c4
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2021-05-04 09:22:13 -0400

    Add files via upload

--- installgitupdate.sh
+++ installgitupdate.sh
@@ -1,36 +1,78 @@
 #!/bin/bash
 
 #download files from vcard-update-scripts Git
-#create working copy directory
-#mv files from git directory to working copy directory
-#make scripts in working directory executable
-#check for gitcheck.sh
-if [ -e /home/pi/testing/gitcheck.sh ];
+if git clone https://github.com/dchap1381/vcard-update-scripts;
 	then
-		#gitcheck.sh found
-		echo "gitcheck.sh found!"
-			#test making gitcheck.sh executable
-			if chmod +x /home/pi/testing/gitcheck.sh;
-				then
-					#gitcheck.sh successfully set to run at boot
-					echo "gitcheck.sh is now executable!"
-				else
-					#gitcheck.sh not successsfully set to run at boot
-					echo "gitcheck.sh is NOT executable!!!. Please try again."
-			fi
+		echo "vcard-update-scripts Git clone successful!"
 	else
-		#gitcheck.sh not found
-		echo "gitcheck.sh NOT found!!! Please try again!!!"
+		echo "vcard-update-scripts Git clone NOT successful!!! Please try again."
+		exit 1
+fi
+
+#create working copy directory
+if mkdir /home/pi/update-scripts-wdir/;
+	then
+		echo "update-scripts working directory created!"
+	else
+		echo "update-scripts working directory NOT created!!! Please try again."
+		exit 1
+fi
+
+#mv files from git directory to working copy directory
+if mv /home/pi/vcard-update-scripts/gitcheck.sh /home/pi/update-scripts-wdir/;
+	then
+		echo "move gitcheck.sh to working directory successful!"
+	else
+		echo "move gitcheck.sh to working directory NOT successful!!! Please try again."
+		exit 1
+		
+fi
+
+if [ -e /home/pi/vcard-update-scripts/updateinstall.sh ];
+	then
+		if mv /home/pi/vcard-update-scripts/updateinstall.sh /home/pi/update-scripts-wdir/;
+			then
+				echo "move updateinstall.sh to working directory successful!"
+			else
+				echo "move updateinstall.sh to working directory NOT successful!!! No update available"
+		fi
+	else
+		echo "updateinstall.sh NOT found. No new updates currently available!!!"
+fi
+
+#make scripts in working directory executable
+if [ -e /home/pi/update-scripts-wdir/gitcheck.sh ];
+	then
+		if chmod +x /home/pi/update-scripts-wdir/gitcheck.sh;
+			then
+				echo "make gitcheck.sh executable successful!"
+			else
+				echo "make gitcheck.sh executable NOT successful!!! Please try again."
+		fi
+	else
+		echo "gitcheck.sh NOT found!!! Please try again."
+fi
+
+if [ -e /home/pi/update-scripts-wdir/updateinstall.sh ];
+	then
+		if chmod +x /home/pi/update-scripts-wdir/updateinstall.sh;
+			then
+				echo "make updateinstall.sh executable successful!"
+			else
+				echo "make updateinstall.sh executable NOT successful!!! Please try again."
+		fi
+	else
+		echo "updateinstall.sh NOT found!!! No new updates available."
 fi
 
 #check if gitcheck.sh autostart exists
-if grep "/home/pi/testing/gitcheck.sh" /etc/xdg/lxsession/LXDE-pi/autostart;
+if grep "/home/pi/update-scripts-wdir/gitcheck.sh" /etc/xdg/lxsession/LXDE-pi/autostart;
 	then
 		#gitcheck.sh already set to run at boot
 		echo "gitcheck.sh already set to run at boot!"
 	else
 		#gitcheck.sh not already set to run at boot
-		if echo "/home/pi/testing/gitcheck.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart;
+		if echo "/home/pi/update-scripts-wdir/gitcheck.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart;
 			then
 				#set to run at boot
 				echo "gitcheck.sh set to run at boot!"
