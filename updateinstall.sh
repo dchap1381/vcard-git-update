f35bbbc17fbf4d90c247f5586435c7175c6e90b9
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2021-05-06 10:41:54 -0400

    Create updateinstall.sh

---
+++ updateinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -e /home/pi/Desktop/testfile.txt ];
+  then
+    echo "testfile.txt does exist! Nothing to do!"
+  else
+    echo "testfile.txt does NOT exist!!! Proceeding."
+    if touch /home/pi/Desktop/testfile.txt;
+      then
+        echo "testfile.txt created!"
+      else
+        echo "testfile.txt NOT created!!!"
+    fi
+fi
