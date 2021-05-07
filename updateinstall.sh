d4bffd4eeb7b77865c4f5bd947d3c34a88192d6c
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2021-05-07 09:32:27 -0400

    Update updateinstall.sh

--- updateinstall.sh
+++ updateinstall.sh
@@ -1,16 +1,113 @@
 #!/bin/bash
-#testing modifications
-#testing mods again
 
-if [ -e /home/pi/Desktop/testfile.txt ];
-  then
-    echo "testfile.txt does exist! Nothing to do!"
-  else
-    echo "testfile.txt does NOT exist!!! Proceeding."
-    if touch /home/pi/Desktop/testfile.txt;
-      then
-        echo "testfile.txt created!"
-      else
-        echo "testfile.txt NOT created!!!"
-    fi
+check_vcardstartsh_autostart()	{
+	grep /home/pi/vcardstart.sh /etc/xdg/lxsession/LXDE-pi/autostart
+}
+
+check_vcarddesktop_shortcut()	{
+	[ -e .config/autostart/vcard.desktop ]
+}
+
+check_vcardstartsh_exists()	{
+	[ -e /home/pi/vcardstart.sh ]
+}
+
+chmod_vcardstartsh()	{
+	chmod +x /home/pi/vcardstart.sh
+}
+
+rm_vcarddesktop_shortcut()	{
+	rm /home/pi/.config/autostart/vcard.desktop
+}
+
+chown_vcardstartsh()	{
+	chown pi:pi /home/pi/vcardstartsh
+}
+
+create_vcardstartsh()	{
+	cat >> /home/pi/vcardstart.sh << EOF
+#!/bin/bash
+
+#function to restart vCard POS
+while :
+	do
+		if pgrep vcardpos.sh;
+			then
+				echo "POS Running!"
+				sleep 5
+			else
+				echo "POS NOT Running!!!"
+				/home/pi/vcard/vcardpos.sh
+				sleep 5
+		fi
+done
+EOF
+}
+
+create_vcardstartsh_autostart()	{
+	cat >> /etc/xdg/lxsession/LXDE-pi/autostart << EOF
+/home/pi/vcardstart.sh
+EOF
+}
+
+# check if old vcard.desktop autostart exists
+if check_vcarddesktop_shortcut;
+	then
+		echo "old vcard.desktop autostart shortcut does exist!"
+	else
+		echo "old vcard.desktop autostart shortcut does NOT exist!!!"
+		# remove old vcard.desktop autostart
+		if rm_vcarddesktop_shortcut;
+			then
+				echo "remove old vcard.dekstop autostart shortcut successful!"
+			else
+				echo "remove old vcard.desktop autostart shortcut NOT successful!!!"
+		fi
+fi
+
+									
+# check if vcardstart autostart shortcut exists
+if check_vcardstartsh_autostart;
+	then
+		echo "vcardstart.sh autostart shortcut does exist!"
+	else
+		echo "vcardstart.sh autostart shortcut does NOT exist!!!"
+		#create vcardstart link in autostart
+		if create_vcardstartsh_autostart;
+			then
+				echo "create vcardstart.sh autostart successful!"
+			else
+				echo "create vcardstart.sh autostart NOT successful!!!"
+		fi
+fi
+
+# check if vcardstart.sh exists
+if check_vcardstartsh_exists;
+	then
+		# vcardstart.sh does exist
+		echo "vcardstart.sh does exist!"
+	else
+		# vcardstart.sh does not exist
+		echo "vcardstart.sh does NOT exist!!!"
+			# create vcardstart.sh file
+			if create_vcardstartsh;
+				then
+					echo "vcardstart.sh created!"
+					#chmod vcardstart.sh
+					if chmod_vcardstartsh;
+						then
+							echo "vcardstart.sh executable!"
+							#chown vcardstart.sh
+							if chown_vcardstartsh;
+								then
+									echo "change ownership of vcardstart.sh successful!"
+								else
+									echo "cahnge ownership of vcardstart.sh NOT successfull!!!"
+							fi
+						else
+							echo "vcardstart.sh NOT executable!!!"
+					fi
+				else
+					echo "vcardstart.sh NOT successful!!!"
+			fi
 fi
