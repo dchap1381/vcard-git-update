55172545e08494272b321151f18b91acca5950ac
Davis Chappell <64147519+dchap1381@users.noreply.github.com>
2021-05-10 15:09:57 -0400

    Update gitcheck.sh

--- gitcheck.sh
+++ gitcheck.sh
@@ -72,6 +72,26 @@ run_updateinstall()	{
 	/home/pi/update-scripts-wdir/updateinstall.sh
 }
 
+diff_gitcheck()	{
+	diff /home/pi/vcard-git-update/gitcheck.sh /home/pi/update-scripts-wdir/gitcheck.sh
+}
+
+diff_updateinstall()	{
+	diff /home/pi/vcard-git-update/updateinstall.sh /home/pi/update-scripts-wdir/updateinstall.sh
+}
+
+chown_gitcheck()	{
+	chown pi:pi /home/pi/update-scripts-wdir/gitcheck.sh
+}
+
+chown_updateinstall()	{
+	chown pi:pi /home/pi/update-scripts-wdirupdateinstall.sh
+}
+
+#################
+#START OF SCRIPT#
+#################
+
 #cd into Git directory before pull
 if cd_vcard_git_update;
 	then
@@ -104,7 +124,7 @@ if git_pull;
 fi
 
 #cd into update-scripts-wdir working directory
-if cd_update_scripts_wdir;
+if cd_update_scripts_wdir; 
 	then
 		echo "cd into update-scripts-wdir successful!"
 	else
@@ -113,82 +133,72 @@ fi
 
 #check if gitcheck.sh exists
 if gitcheck_exists;
+then
+	echo "gitcheck.sh found."
+	if diff_gitcheck;
 	then
-		echo "gitcheck.sh found!"
-		if md5check_gitcheck;
-			then
-				echo "changes to gitcheck NOT found!!! Keeping copy in working directory."
-			else
-				echo "changes to gitcheck found!"
-				if md5sum_gitcheck;
-					then
-						echo "New gitcheck md5sum successful"
-						if chown_gitcheck_md5sum;
-							then
-								echo "change ownership of gitcheck md5sum successful!"
-								if cp_gitcheck;
-									then
-										echo "copy gitcheck to working directory successful!"
-										if chmod_gitcheck;
-											then
-												echo "make gitcheck executable successful!"
-											else 
-												echo "make gitcheck executable NOT successful!!!"
-										fi
-									else
-										echo "copy gitcheck to working directory NOT successful!!!"
-								fi
-							else
-								echo "change ownership of gitcheck md5sum NOT successful!!!"
-						fi
-				        else
-						echo "New gitcheck md5sum NOT successful!!!"
-				fi
-		fi
+		echo "no changes to gitcheck.sh found. nothing to do."
 	else
-		echo "gitcheck.sh NOT found!!!"
+		echo "changes to gitcheck.sh found."
+		if cp_gitcheck;
+		then
+			echo "copy gitcheck.sh to working directroy successful."
+			if chown_gitcheck;
+			then
+				echo "change ownership of gitcheck.sh to user pi successful"
+				if chmod_gitcheck;
+				then
+					echo "make gitcheck.sh executbale successful."
+				else
+					echo "make gitcheck.sh execuabtle NOT successful!!!"
+				fi
+			else
+				echo "change ownership of gitcheck.sh NOT successful!!!"
+			fi
+		else
+			echo "copy gitcheck.sh to working directory NOT successful!!!"
+		fi
+	fi
+else
+	echo "gitcheck.sh NOT found!!! Something went wrong. Please try again."
 fi
 
-#check if updateinstall exists
+#check if updateinstall.sh exists
 if updateinstall_exists;
+then
+	echo "updateinstall.sh found."
+	if diff_updateinstall;
 	then
-		echo "updateinstall found!"
-		if md5check_updateinstall;
-			then
-				echo "changes to updateinstall NOT found!!! Keeping copy in working directory"
-			else
-				echo "changes to updateinstall found!"
-				if md5sum_updateinstall;
-					then
-						echo "new updateinstall md5sum successful!"
-						if chown_updateinstall_md5sum;
-							then
-								echo "change ownership of updateinstall md5sum successful!"
-								if cp_updateinstall;
-									then
-										echo "copy updateinstall to working directory successful!"
-										if chmod_updatinstall;
-											then
-												echo "make updateinstall executable successful!"
-												if run_updateinstall;
-													then
-														echo "executing updateinstall successful!"
-													else
-														echo "executing updateinstall NOT successful!!! updates not applied!!!"
-												fi
-											else
-												echo "make updateisntall executable NOT successful!!!"
-										fi
-									else
-										echo " copy updateinstall to working directory NOT successful!!!"
-								fi
-							else
-								echo "change ownershuo of updateinstall md5sum NOT successful!!!"
-						fi
-					else
-						echo "new updateinstall md5sum NOT successful!!!"
-				fi
-		fi
+		echo "no chages to updateinstall.sh found. keeping copy in working directory"
 	else
-		echo "udpateinstall NOT found!!!"
-fi
+		echo "changes to updateinstall.sh found."
+		if cp_updateinstall;
+		then
+			echo "copy updateinstall.sh to working directory successful."
+			if chown_updateinstall;
+			then
+				echo "change ownership of updateinstall.sh to user pi succesful."
+				if chmod_updatinstall;
+				then
+					echo "make updateinstall.sh executable successful."
+					if run_updateinstall;
+					then
+						echo "executing updateinstall.sh successful."
+					else
+						echo "executing updateinstall.sh NOT sucessful!!!"
+					fi
+				else
+					echo "makee updateinstall.sh executable NOT successful!!!"
+				fi
+			else
+				echo "change ownership of updateinstall.sh to user pi NOT successful!!!"
+			fi
+		else
+			echo "copy updateinstall.sh to working directory NOT successful!!!"
+		fi
+	fi
+else
+	echo "updateinstall.sh NOT found!!! Something went wrong. Please try again."
+
+sleep 3
+read -p "press enter to continue" -r
